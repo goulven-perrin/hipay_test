c6fa17dbd756a619fc4dbeb9cbfdf1c03f2603fc
Perrin_Goulven <112887119+goulven-perrin@users.noreply.github.com>
2023-09-20 09:03:37 +0200

    ListAllPlant
    Test sur l'API Trefle.io
    Récupération de l'ensemble des données "plants"

---
+++ BackEnd/V1/list.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+// Définir le token dans une variable
+const token = '8fFX4FwH76RP4Y1GesjAJ_XP-kIXFCFqH-nkFeQWis0';
+
+// Fonction pour effectuer la recherche et afficher toutes les informations des plantes
+function searchPlants() {
+    // Effectuer la recherche en utilisant l'entrée de l'utilisateur
+    axios.get(`https://trefle.io/api/v1/plants?token=${token}`)
+        .then(response => {
+            const plantData = response.data.data;
+            console.log(plantData);
+        })
+        .catch(error => {
+            console.error('Une erreur s\'est produite lors de la recherche de plantes :', error);
+        });
+}
+
+// Démarrer la recherche initiale
+searchPlants();
