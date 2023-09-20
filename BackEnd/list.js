60425bf15bd496e22dedda9b5c06ffe0a4fe14ef
Perrin_Goulven <112887119+goulven-perrin@users.noreply.github.com>
2023-09-20 09:10:30 +0200

    Filtre
    Test du filtre selon les information donnée par la documentation trefle

--- BackEnd/list.js
+++ BackEnd/list.js
@@ -5,7 +5,7 @@ const token = '8fFX4FwH76RP4Y1GesjAJ_XP-kIXFCFqH-nkFeQWis0';
 // Fonction pour effectuer la recherche et afficher toutes les informations des plantes
 function searchPlants() {
     // Effectuer la recherche en utilisant l'entrée de l'utilisateur
-    axios.get(`https://trefle.io/api/v1/plants?token=${token}`)
+    axios.get(`https://trefle.io/api/v1/plants?token=${token}&page=1&filter[family_common_name]=pine%20family`)
         .then(response => {
             const plantData = response.data.data;
             console.log(plantData);
